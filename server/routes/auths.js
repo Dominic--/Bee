f5823ead6562c5e5ebd37ec681e123b9853546ff
root <root@ccpt.cc>
2014-10-14 07:39:02 +0100

    Authorize is successful, access_token is got

--- server/routes/auths.js
+++ server/routes/auths.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var http = require('http');
+var http = require('https');
+var request = require('request');
 
 router.get('/', function(req, res) {
 	var options = {
@@ -9,12 +10,21 @@ router.get('/', function(req, res) {
 	};
 
 	var callback = function(response) {
+		var str = '';
+		response.on('data', function (chunk) {
+			str += chunk;
+		});
+
+		response.on('end', function() {
+//			console.log(str);
+			res.send(str);
+		});
 		console.log('start auth...');
 	};
 
 	http.request(options, callback).end();
 
-  	res.send('auth...');
+  	//res.send('auth...');
 });
 
 
@@ -22,16 +32,19 @@ router.get('/redirect', function(req, res) {
 	var req_code = req.param('code');
 
 	request.post(
-		'http://openapi.youku.com/v2/oauth2/token',
-		{ json: { 
+		'https://openapi.youku.com/v2/oauth2/token',
+		{form:{
 					client_id:'e342c22c049a5404',
 					client_secret:'faa1831867ddf26c496cd6365ca44ea2',
 					grant_type:'authorization_code',
 					code:req_code,
 					redirect_uri:'http://www.ccpt.cc:3000/auths/redirect'
-				}
-		},
+		
+		}},
+
 		function (error, response, body) {
+			console.log(body);
+			
 			if (!error && response.statusCode == 200) {
 				console.log(body);
 			}
@@ -39,7 +52,6 @@ router.get('/redirect', function(req, res) {
 		);
 
 	console.log('auth redirect is active...');
-	res.send('authredirect...')
 });
 
 router.get('/authcomplete', function(req, res) {
